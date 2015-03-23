b04bee32a3d440b8c3ef86c57413d69c2b68a012
Jamie Haskell <jmehaskell@gmail.com>
2015-03-23 15:23:37 -0500

    check if user is already logged in, added ability to logout

--- public/authorized.php
+++ public/authorized.php
@@ -1,5 +1,16 @@
 <?php
 	var_dump($_POST);
+	
+	session_start();
+	$username = $_SESSION['LOGGED_IN_USER'];
+
+	if(isset($_SESSION['LOGGED_IN_USER'])){
+		echo $username;
+	} else {
+		header("Location: login.php");
+		exit();
+	};
+
 ?>
 
 <!DOCTYPE html>
@@ -9,5 +20,8 @@
 </head>
 <body>
 	<h2>Access Authorized</h2>
+	<form action="logout.php">
+		<input type='submit' value="Logout">
+	</form>
 </body>
 </html>
--- public/login.php
+++ public/login.php
@@ -1,16 +1,35 @@
 <?php
+	
+	// Start a session for the page
+	session_start();
+
+	// If user is already logged in, redirect to authorization page
+	if(isset($_SESSION['LOGGED_IN_USER'])){
+		header("Location: authorized.php");		// header() is a redirect function
+		exit();
+	}
+	
+	// If user is not logged in, ask for credentials
 	$username = "guest";
 	$password = "password";
 	$message = "";
 
-	if(isset($_POST['username']) && isset($_POST['password'])){
+	if (isset($_POST['username']) && isset($_POST['password'])){
 		$inputName = $_POST['username'];
 		$inputPswd = $_POST['password'];
 
 		if($inputName != $username || $inputPswd != $password){
 			$message = "Wrong username or password.";
 		} else {
-			header("Location: authorized.php?");		// header() is a redirect function
+			// Initialize a session
+			// session_start();
+			// clear array of any data from previous sessions
+			$_SESSION = array();
+			// store user's username to pass to next page
+			$_SESSION['LOGGED_IN_USER'] = $username;
+
+			// redirect to authorization page and exit() any remaining PHP script
+			header("Location: authorized.php");		// header() is a redirect function
 			exit();
 		}
 	} else {
