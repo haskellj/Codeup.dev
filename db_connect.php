696869eccb20e583facdf1445b6ca0cfa0d3c21a
Jamie Haskell <jmehaskell@gmail.com>
2015-04-02 15:01:15 -0500

    added a large example block from Ben on how to create and insert SQL

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,11 @@
 <?php
 	// Get new instance of PDO object and point it toward the 'employees' database
 	// Note: the password has been left ambiguous for github commits
-	$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'password');
+	$dbc = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
 
 	// Tell PDO to throw exceptions on error, rather than failing silently
 	$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	// Display the PDO connection status
+	echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
--- pdo_test.php
+++ pdo_test.php
@@ -1,6 +1,46 @@
 <?php
+	// Define constants to be used in the PDO call before the db_connect.php file is required
+	// Note: 'password' has been left ambiguous for the github commit
+	define ('DB_HOST', '127.0.0.1');
+	define ('DB_NAME', 'employees');
+	define ('DB_USER', 'codeup');
+	define ('DB_PASS', 'password');
 
 	require 'db_connect.php';
-	
-	// Display the PDO connection status
-	echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+
+	$query = 'CREATE TABLE IF NOT EXISTS users (
+		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	    email VARCHAR(240) NOT NULL,
+	    name  VARCHAR(50) NOT NULL,
+	    PRIMARY KEY (id), 
+	    CONSTRAINT user_email_unq UNIQUE (email)	-- avoids duplicate entries of artist-album name combinations
+		)';
+
+	$dbc->exec($query);		// Executes the code within $query and returns the number of rows affected
+
+	$query = "INSERT INTO users (email, name)
+				VALUES ('ben@codeup.com,'Ben Batschelet')";
+
+	// Execute Query, save the result as a variable
+	$numRows = $dbc->exec($query);
+	echo "Inserted $numRows row(s).".PHP_EOL;
+
+	$users = [
+		['email' => 'jason@codeup.com', 'name' => 'Jason Straughn'],
+		['email' => 'michael@codeup.com', 'name' => 'Michael Girdley'],
+		['email' => 'chris@codeup.com', 'name' => 'Chris Turner']
+	];
+
+	foreach ($users as $user) {
+		$query = "INSERT INTO users (email, name) VALUES ('{$user['email']}', '{$user['name']}')";
+
+		$dbc->exec($query);
+
+		echo "Inserted id ";
+	}
+
+
+
+
+
