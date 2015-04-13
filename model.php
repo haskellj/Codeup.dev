7bc28794b4a2e03ca0d833e230f80e2c2c96e88a
Jamie Haskell <jmehaskell@gmail.com>
2015-04-13 15:54:46 -0500

    created connection to database, nothing else works

--- model.php
+++ model.php
@@ -1,11 +1,11 @@
 <?php
 	
 	define ('DB_HOST', '127.0.0.1');
-	define ('DB_NAME', '');
-	define ('DB_USER', '');
+	define ('DB_NAME', 'parks_db');
+	define ('DB_USER', 'parks_user');
 	define ('DB_PASS', 'password');
 
-	require_once 'db_connect.php';
+
 
 	class Model
 	{
@@ -20,92 +20,104 @@
 	        self::dbConnect();
 	    }
 
-	    // Connect to the database
+	    // Connect to the database, if it hasn't been connected to already.
 	    private static function dbConnect()
 	    {
 	        if (!self::$dbc)
 	        {
-	            // @TODO: Connect to database
-	        }
+	    		self::$dbc = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+
+				// Tell PDO to throw exceptions on error, rather than failing silently
+				self::$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+				// // Display the PDO connection status
+				self::$dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+	    		echo 'You are connected to the database';
+	        } else {
+	        	echo 'Connection to the database failed';
+	    	}
 	    }
 
 		// Magic setter to populate the $attributes array
-		public function __set($name, $value)
-		{
-			// Create key/value pairs by setting $name key to hold the $value in the array
-			$this->attributes[$name] = $value;
-		}
+		// public function __set($name, $value)
+		// {
+		// 	// Create key/value pairs by setting $name key to hold the $value in the array
+		// 	$this->attributes[$name] = $value;
+		// }
 
-		// Magic getter to retrieve values from the attributes array based on the key name, provided it exists
-		public function __get($name)
-		{
-			if(array_key_exists($name, $this->attributes)) {
-				return $this->attributes[$name];
-			}
+		// // Magic getter to retrieve values from the attributes array based on the key name, provided it exists
+		// public function __get($name)
+		// {
+		// 	if(array_key_exists($name, $this->attributes)) {
+		// 		return $this->attributes[$name];
+		// 	}
 
-			return null;
-		}
+		// 	return null;
+		// }
 
-		public static function getTableName()
-		{
-			return static::$table;
-		}
+		// public static function getTableName()
+		// {
+		// 	return static::$table;
+		// }
 
-	    // Persist the object to the database
-	    public function save()
-	    {
-	        // @TODO: Ensure there are attributes before attempting to save
+	 //    // Persist the object to the database
+	 //    public function save()
+	 //    {
+	 //        // Ensure there are attributes before attempting to save
+	 //        if (!empty($this->attributes)) {
 
-	        // @TODO: Perform the proper action - if the `id` is set, this is an update, if not it is a insert
+		//         // Perform the proper action - if the `id` is set, this is an update, if not it is a insert
+		//         // If inserting new id, add the id to the attributes array so the object can properly reflect the id as well
+	 //        	if(isset($this->id)){
+	 //        		$query = "UPDATE national_parks
+		// 						SET location=".$this->location.", date_established=".$this->date_established.", 
+		// 							area_in_acres=".$this->area_in_acres.", description=".$this->description.
+		// 						"WHERE id=".$this->id;
+	 //        	} else {
+	 //        		$query = "INSERT INTO national_parks (name, location, date_established, area_in_acres, description)
+		// 						VALUES (:name, :location, :date_est, :area, :description)";
+		// 			$this->id = $this->id;
+	 //        	}
 
-	        // @TODO: Ensure that update is properly handled with the id key
+	 //        	// You will need to iterate through all the attributes to build the prepared query
+	 //        	foreach($this->attributes as $attribute) {
 
-	        // @TODO: After insert, add the id back to the attributes array so the object can properly reflect the id
+	 //        	}
 
-	        // @TODO: You will need to iterate through all the attributes to build the prepared query
+		//         // Use prepared statements to ensure data security
+		//         $insert = $dbc->prepare($query);
+		// 	}
 
-	        // @TODO: Use prepared statements to ensure data security
+	 //    // Find a record based on an id
+	 //    public static function find($id)
+	 //    {
+	 //        // Get connection to the database
+	 //        self::dbConnect();
 
+	 //        // @TODO: Create select statement using prepared statements
 
-	    // Find a record based on an id
-	    public static function find($id)
-	    {
-	        // Get connection to the database
-	        self::dbConnect();
+	 //        // @TODO: Store the resultset in a variable named $result
 
-	        // @TODO: Create select statement using prepared statements
+	 //        // The following code will set the attributes on the calling object based on the result variable's contents
 
-	        // @TODO: Store the resultset in a variable named $result
+	 //        $instance = null;
+	 //        if ($result)
+	 //        {
+	 //            $instance = new static;
+	 //            $instance->attributes = $result;
+	 //        }
+	 //        return $instance;
+	 //    }
 
-	        // The following code will set the attributes on the calling object based on the result variable's contents
+	 //    // Find all records in a table
+	 //    public static function all()
+	 //    {
+	 //        self::dbConnect();
 
-	        $instance = null;
-	        if ($result)
-	        {
-	            $instance = new static;
-	            $instance->attributes = $result;
-	        }
-	        return $instance;
-	    }
-
-	    // Find all records in a table
-	    public static function all()
-	    {
-	        self::dbConnect();
-
-	        // @TODO: Learning from the previous method, return all the matching records
-	    }
+	 //        // @TODO: Learning from the previous method, return all the matching records
+	 //    }
 
 	}
 
 	// Test class:
-	// $new = new Model();
-	// $new->name = 'Sir Test';
-	// $new->group = 'Knights of the CodeTable';
-	// $new->age = 35;
-	// $new->motto = 'This code shall not pass!';
-
-	// echo $new->name.PHP_EOL;
-	// echo $new->group.PHP_EOL;
-	// echo $new->age.PHP_EOL;
-	// echo $new->motto.PHP_EOL;
+	$aishaTyler = new Model();
