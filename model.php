31584a820eddd245d79ebcf21190b729d2c9a60c
Jamie Haskell <jmehaskell@gmail.com>
2015-04-13 11:08:27 -0500

    created Model class, not tested yet

---
+++ model.php
@@ -0,0 +1,23 @@
+<?php
+	
+	class Model
+	{
+		private $attributes = array();
+
+		// Magic setter to populate the $attributes array
+		public function __set($name, $value)
+		{
+			// Create key/value pairs by setting $name key to hold the $value in the array
+			$this->attributes[$name] = $value;
+		}
+
+		// Magic getter to retrieve values from the attributes array based on the key name, provided it exists
+		public function __get($name)
+		{
+			if(array_key_exists($name, $this->attributes)) {
+				return $this->attributes[$name];
+			}
+
+			return null;
+		}
+	}
